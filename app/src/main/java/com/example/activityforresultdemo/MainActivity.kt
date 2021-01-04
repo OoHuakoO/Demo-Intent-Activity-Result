6f8089b7a8d835998f372d5fff59ff0c3d18d899
huak <komsark_@hotmail.co.th>
2021-01-04 22:16:04 +0700

    commmit

--- app/src/main/java/com/example/activityforresultdemo/MainActivity.kt
+++ app/src/main/java/com/example/activityforresultdemo/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.activityforresultdemo
-
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +10,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     companion object{
     private const val FIRST_ACTIVITY_REQUEST_CODE = 1
+        private const val SECOND_ACTIVITY_REQUEST_CODE = 2
+        const val NAME = "name"
+        const val EMAIL = "email"
 }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,23 +21,30 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,FirstActivity::class.java)
             startActivityForResult(intent, FIRST_ACTIVITY_REQUEST_CODE)
         }
+        btn_launch_activity_second.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivityForResult(intent,SECOND_ACTIVITY_REQUEST_CODE)
+        }
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK)
         {
             if(requestCode == FIRST_ACTIVITY_REQUEST_CODE){
                 tv_first_activity_result.text = "First Activity Result Success"
+                Log.e("Success","OK")
+            }else if(requestCode == SECOND_ACTIVITY_REQUEST_CODE){
+                if(data!=null){
+                    val name = data.getStringExtra(NAME)
+                    val email = data.getStringExtra(EMAIL)
+                    tv_second_activity_result.text = "$name => $email"
+                }
             }
         }
         else if(resultCode == Activity.RESULT_CANCELED){
             Log.e("Cancelled","Cancelled")
             Toast.makeText(this@MainActivity,
                     "ResultCancelled",Toast.LENGTH_SHORT).show()
-
         }
     }
-
-
 }
